095c5dfabcb452ca988eb9c2a76cad921ab95793
Jesse Egbosionu <j3bsie@gmail.com>
2021-04-15 11:41:41 +0100

    Refactor - More refactoring

--- goalservice/models.py
+++ goalservice/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from utils.helpers import BaseAbstractModel
-# Create your models here.
 
 
 class GoalManager(models.Manager):
@@ -28,40 +27,29 @@ class GoalQuerySet(models.QuerySet):
 
 
 class Goal(BaseAbstractModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,
-                             default=None, null=True, related_name='personal_todo')
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('userservice.User', on_delete=models.CASCADE,
+                             default=None, null=True, related_name='goals')
     goal_name = models.CharField(max_length=100, null=False)
-    category = models.ForeignKey(GoalCategory, on_delete=models.CASCADE)
-    amount = models.BigIntegerField()
-    amount_generated = models.BigIntegerField(default=0)
-    amount_withdrawn = models.BigIntegerField(default=0)
-    currency = models.CharField(
+    goal_category = models.ForeignKey(GoalCategory, on_delete=models.CASCADE)
+    goal_amount = models.BigIntegerField()
+    goal_amount_generated = models.BigIntegerField(default=0)
+    goal_amount_withdrawn = models.BigIntegerField(default=0)
+    goal_currency = models.CharField(
         max_length=10, choices=CURRENCIES, default='NGN')
-    status = models.CharField(
-        max_length=50,
-        choices=[(Goal, Goal.value) for Goal in GoalStatus],
-        default=GoalStatus.running
-    )
-    share_code = models.CharField(
-        null=True, max_length=LENGTH_OF_ID, unique=True)
-    collection_code = models.CharField(null=True, max_length=16)
-    start_date = models.DateField(auto_now_add=True)
+    goal_code = models.CharField(max_length=15, unique=True)
+    goal_start_date = models.DateTimeField()
+    goal_completion_date = models.DateTimeField()
     is_visible = models.BooleanField(default=False)
-    goal_description = models.TextField(
-        null=True, default=default_Goal_description)
-    updated_at = models.DateTimeField(auto_now=True)
-    Goal_duration = models.CharField(
+    goal_description = models.TextField(null=True, default=default_Goal_description)
+    goal_duration = models.CharField(
         max_length=50,
         choices=[(Goal, Goal.value) for Goal in GoalDuration])
-    Goal_media = models.URLField(null=True)
-    completion_date = models.DateTimeField()
     objects = GoalManager()
-    Goal_status = GoalQuerySet().as_manager()
+    goal_state = GoalQuerySet().as_manager()
 
 
 class GoalContribution(BaseAbstractModel):
-    Goal_code = models.ForeignKey(
+    goal_code = models.ForeignKey(
         Goal, to_field='share_code', db_column='Goal_code', on_delete=models.CASCADE)
     contributor_name = models.CharField(max_length=100)
     contributor_email = models.EmailField(max_length=100)
