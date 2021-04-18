186cf06d1dbe30c74269c675373dbc2cffcfd40d
Jesse Egbosionu <j3bsie@gmail.com>
2021-04-18 05:25:20 +0100

    Refactor - Start Afresh

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 """config URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -12,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path("api/v1/", include("userservice.urls")),
-    path("api/v1/", include("savingservice.urls")),
-    path("api/v1/", include("adminservice.urls")),
-    path("api/v1/", include("walletservice.urls")),
+    path('admin/', admin.site.urls),
 ]
--- userservice/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserserviceConfig(AppConfig):
-    name = 'userservice'
-
-    def ready(self):
-        import userservice.signals
